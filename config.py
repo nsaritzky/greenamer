c1ed693f170132c5acddbcc47ff07fe3632808b4
Nathan Saritzky <nsaritzky@gmail.com>
2018-04-24 00:43:58 -0700

    Removed CDN_DOMAIN from .env

--- config.py
+++ config.py
@@ -24,5 +24,3 @@ class Config(object):
         redirect_url = 'http://localhost:5000'
     OAUTH_URL = 'https://www.strava.com/oauth/authorize?client_id=24713&response_type=code&redirect_uri={0}/auth'.format(
         redirect_url)
-
-    CDN_DOMAIN = 'greenamer.s3.amazonaws.com'
