704cf493bb6dd644d9130b65a95949e6f2535173
Nathan Saritzky <nsaritzky@gmail.com>
2019-12-03 15:51:24 -0800

    - Reformatted routes files - Added script to .gitignore

--- app/webhooks/routes.py
+++ app/webhooks/routes.py
@@ -8,29 +8,33 @@ import json
 
 import requests
 
-# fasdfsadasdfas
 
-
-@bp.route('/handler', methods=['GET', 'POST'])
+@bp.route("/handler", methods=["GET", "POST"])
 def webhook_handler():
-    current_app.logger.debug(request.data)
-    if request.method == 'GET':
-        token = request.args.get('hub.verify_token')
-        challenge = request.args.get('hub.challenge')
-        current_app.logger.info('Webhook subscription request got a response with verify token {}'.format(token))
+    current_app.logger.debug(request.get_data())
+    if request.method == "GET":
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+        current_app.logger.info(
+            f"Webhook subscription request got a response with verify token {token}"
+        )
         if token == Config.WEBHOOK_TOKEN:
-            current_app.logger.info('Token accepted, sending challenge back')
-            payload = json.dumps({'hub.challenge': challenge})
+            current_app.logger.info("Token accepted, sending challenge back")
+            payload = json.dumps({"hub.challenge": challenge})
             return payload, 200
-    elif request.method == 'POST':
-        current_app.logger.debug(request.data)
-        data = request.get_json()
-        if data['aspect_type'] == 'create':
-            athlete = User.query.get(int(data['owner_id']))
+    elif request.method == "POST":
+        current_app.logger.debug(request.get_data())
+        data = request.get_json(force=True)
+        if data["aspect_type"] == "create":
+            athlete = User.query.get(int(data["owner_id"]))
             if athlete is not None:
-                athlete.resolve_webhook(data['object_id'])
-                current_app.logger.info('Strava webhook received: User {}, Activity {}'.format(athlete.id, data['object_id']))
+                athlete.resolve_webhook(data["object_id"])
+                current_app.logger.info(
+                    f"Strava webhook received: User {athlete.id}, Activity {data['object_id']}"
+                )
             else:
-                current_app.logger.warning('You got a webhook for {}, but they weren\'t in the database'.format(data['owner_id']))
-            return '', 200
-    return '', 200
+                current_app.logger.warning(
+                    f"You got a webhook for {data['owner_id']}, but they weren't in the database"
+                )
+            return "", 200
+    return "", 200
